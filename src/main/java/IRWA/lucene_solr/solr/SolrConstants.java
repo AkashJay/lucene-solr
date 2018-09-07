cc6f2ef39d63329ecb37796cd4a06fd17cbb6819
AkashJay <akash/jayaweera@gmail.com>
2018-09-07 14:03:47 +0530

    Implement Constant class Implement Constant class to catch the fields in the core

---
+++ src/main/java/IRWA/lucene_solr/solr/SolrConstants.java
@@ -0,0 +1,36 @@
+package IRWA.lucene_solr.solr;
+
+public class SolrConstants {
+	
+	public static final String isbn = "isbn";
+	
+	public static final String title = "title";
+	
+	public static final String pubname ="pubname";
+	
+	public static final String pub_location = "pub_location";
+	
+	public static final String  pub_latlng = "pub_latlng";
+	
+	public static final String author_firstname = "author_firstname";
+	
+	public static final String author_lastname = "author_lastname";
+	
+	public static final String author_fullname = "author_fullname";
+	
+	public static final String categories = "categories";
+	
+	public static final String wildcard_price = "*_price";
+	
+	public static final String bookstore_amazon = "amazon";
+	
+	public static final String bookstore_infybeam = "infybeam";
+	
+	public static final String bookstore_flipkart = "flipkart";
+	
+	public static final String bookstore_rediff = "rediff";
+	
+	
+	
+
+}
